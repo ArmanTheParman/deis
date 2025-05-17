2890a5b2e9bbf958b70b7483ca5fb88a284257ec
Parman <armantheparman@protonmail.com>
2025-05-18 09:24:17 +1000

    textpatch

--- deis_patches/text_patch.sh
+++ deis_patches/text_patch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-for file in $(find ./ -type f) ; do 
-if ! grep "Please contribute if you find Bitcoin Deis useful. Visit" $file ; then continue ; fi
-gsed -i 's|github\.com/bitcoin/bitcoin|github\.com/armantheparman/deis|g' $file 
+
+clear
+
+if ! which gsed >/dev/null ; then echo "Needs gsed to work. Exiting." ; exit ; fi
+
+for file in $(find ../ -type f) ; do
+ gsed -i "s/Bitcoin Deis/Bitcoin Deis/g" $file
+done
+
+
+for file in $(find ../ -type f) ; do 
+if ! grep -q "Please contribute if you find Bitcoin Deis useful. Visit" $file ; then continue ; fi
+ gsed -i 's|github\.com/bitcoin/bitcoin|github\.com/armantheparman/deis|g' $file 
 done
 
